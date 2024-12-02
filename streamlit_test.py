47c5caae28642a4adc2038d101ef7675cb8d25eb
sdelhaye <sdelhaye@perspective.brussels>
2024-12-02 09:53:32 +0100

    modify size

--- streamlit_test.py
+++ streamlit_test.py
@@ -26,15 +26,7 @@ diff_occ_fin["nomen_db"]=diff_occ_fin["nomen_db"].apply(lambda x: ast.literal_ev
 
 st.set_page_config(layout="wide")
 
-st.markdown("""
-<style>
-.big-font {
-    font-size:300px !important;
-}
-</style>
-""", unsafe_allow_html=True)
 
-st.markdown('<p class="big-font">Hello World !!</p>', unsafe_allow_html=True)
 st.write("SITEX2.0 - Comparaison BD et relevé du BRAT")
 st.write("Ce qu'il nous manque à notre DB")
 general=st.radio("Voulez-vous voir le manque de notre DB selon :",
