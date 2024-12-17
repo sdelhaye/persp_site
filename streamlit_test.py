8cbac5e667dde6a8b7db297b3eac40103a0fbd13
sdelhaye <sdelhaye@perspective.brussels>
2024-12-17 11:52:02 +0100

    test

--- streamlit_test.py
+++ streamlit_test.py
@@ -57,6 +57,8 @@ diff_occ_fin["miss_nomen_db"]=diff_occ_fin["miss_nomen_db"].apply(lambda x: ast.
 diff_occ_fin["nomen_brat"]=diff_occ_fin["nomen_brat"].apply(lambda x: ast.literal_eval(x))
 diff_occ_fin["nomen_db"]=diff_occ_fin["nomen_db"].apply(lambda x: ast.literal_eval(x))
 
+st.write("The date for our DB state is", sitex2_occ_block[sitex2_occ_block["id_bat"]==diff_occ_fin.iloc[0][0]].iloc[0,0])
+
 
 general=st.radio("Voulez-vous voir la comparaison de notre DB selon :",
                ["Le nombre d'occupation", "La superficie plancher" ])
@@ -295,7 +297,7 @@ elif general=="La superficie plancher":
         colname="regroupement_fill"
     else:
         colname="nomen"
-
+    
     data=data[~data['occupcode_'].str.startswith('15')]
     niveau=1
     data["nomen"]=data["occupcode_"].apply(lambda x: x[:2 + 3 * (niveau - 1)] if not pd.isna(x) else np.nan)
